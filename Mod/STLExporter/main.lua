c8c120b3a8c3d9ba80f4c015f8bb4465d66f5a67
leio <zhangleio@outlook.com>
2015-12-02 18:54:00 +0800

    过滤不可见的顶点

--- Mod/STLExporter/main.lua
+++ Mod/STLExporter/main.lua
@@ -89,45 +89,105 @@ local g_dBlockSizeInverse = 1.0 / g_dBlockSize;
 local g_blockSize = g_dBlockSize;
 local g_half_blockSize = g_blockSize*0.5;
 local function Int16x3(x,y,z)
-	return {x,y,z};
+	return {x = x,y = y,z = z};
 end
 local NeighborOfsTable =
 	{
-		Int16x3(0,0,0),			--0	 rbp_center 
-		Int16x3(1,0,0),			--1	 rbp_pX	
-		Int16x3(-1,0,0),			--2	 rbp_nX	
-		Int16x3(0,1,0),			--3	 rbp_pY	
-		Int16x3(0,-1,0),			--4	 rbp_nY	
-		Int16x3(0,0,1),			--5	 rbp_pZ	
-		Int16x3(0,0,-1),			--6	 rbp_nz	
-		Int16x3(1,1,0),			--7	 rbp_pXpY	
-		Int16x3(1,-1,0),			--8	 rbp_pXnY	
-		Int16x3(1,0,1),			--9	 rbp_pXpZ	
-		Int16x3(1,0,-1),			--10 rbp_pXnZ	
-		Int16x3(-1,1,0),			--11 rbp_nXpY	
-		Int16x3(-1,-1,0),		--12 rbp_nXnY	
-		Int16x3(-1,0,1),			--13 rbp_nXpZ	
-		Int16x3(-1,0,-1),		--14 rbp_nXnZ	
-		Int16x3(0,1,1),			--15 rbp_pYpZ	
-		Int16x3(0,1,-1),			--16 rbp_pYnZ	
-		Int16x3(0,-1,1),			--17 rbp_nYpZ	
-		Int16x3(0,-1,-1),		--18 rbp_nYnZ	
-		Int16x3(1,1,1),			--19 rbp_pXpYpZ 
-		Int16x3(1,1,-1),			--20 rbp_pXpYnZ 
-		Int16x3(1,-1,1),			--21 rbp_pXnYPz 
-		Int16x3(1,-1,-1),		--22 rbp_pXnYnZ 
-		Int16x3(-1,1,1),			--23 rbp_nXpYpZ 
-		Int16x3(-1,1,-1),		--24 rbp_nXpYnZ 
-		Int16x3(-1,-1,1),		--25 rbp_nXnYPz 
-		Int16x3(-1,-1,-1),		--26 rbp_nXnYnZ 
+		[0] = Int16x3(0,0,0),			--0	 rbp_center 
+		[1] = Int16x3(1,0,0),			--1	 rbp_pX	
+		[2] = Int16x3(-1,0,0),			--2	 rbp_nX	
+		[3] = Int16x3(0,1,0),			--3	 rbp_pY	
+		[4] = Int16x3(0,-1,0),			--4	 rbp_nY	
+		[5] = Int16x3(0,0,1),			--5	 rbp_pZ	
+		[6] = Int16x3(0,0,-1),			--6	 rbp_nz	
+		[7] = Int16x3(1,1,0),			--7	 rbp_pXpY	
+		[8] = Int16x3(1,-1,0),			--8	 rbp_pXnY	
+		[9] = Int16x3(1,0,1),			--9	 rbp_pXpZ	
+		[10] = Int16x3(1,0,-1),			--10 rbp_pXnZ	
+		[11] = Int16x3(-1,1,0),			--11 rbp_nXpY	
+		[12] = Int16x3(-1,-1,0),		--12 rbp_nXnY	
+		[13] = Int16x3(-1,0,1),			--13 rbp_nXpZ	
+		[14] = Int16x3(-1,0,-1),		--14 rbp_nXnZ	
+		[15] = Int16x3(0,1,1),			--15 rbp_pYpZ	
+		[16] = Int16x3(0,1,-1),			--16 rbp_pYnZ	
+		[17] = Int16x3(0,-1,1),			--17 rbp_nYpZ	
+		[18] = Int16x3(0,-1,-1),		--18 rbp_nYnZ	
+		[19] = Int16x3(1,1,1),			--19 rbp_pXpYpZ 
+		[20] = Int16x3(1,1,-1),			--20 rbp_pXpYnZ 
+		[21] = Int16x3(1,-1,1),			--21 rbp_pXnYPz 
+		[22] = Int16x3(1,-1,-1),		--22 rbp_pXnYnZ 
+		[23] = Int16x3(-1,1,1),			--23 rbp_nXpYpZ 
+		[24] = Int16x3(-1,1,-1),		--24 rbp_nXpYnZ 
+		[25] = Int16x3(-1,-1,1),		--25 rbp_nXnYPz 
+		[26] = Int16x3(-1,-1,-1),		--26 rbp_nXnYnZ 
 	};
+local RelativeBlockPos_Start_Index = -1;
+local function get_next()
+	RelativeBlockPos_Start_Index = RelativeBlockPos_Start_Index + 1;
+	return RelativeBlockPos_Start_Index;
+end
+local rbp_center			= get_next(); --0
+local rbp_pX				= get_next();
+local rbp_nX				= get_next();
+local rbp_pY				= get_next();
+local rbp_nY				= get_next();
+local rbp_pZ				= get_next();
+local rbp_nZ				= get_next();
+
+local rbp_pXpY				= get_next();
+local rbp_pXnY				= get_next();
+local rbp_pXpZ				= get_next();
+local rbp_pXnZ				= get_next();
+
+local rbp_nXpY				= get_next();
+local rbp_nXnY				= get_next();
+local rbp_nXpZ				= get_next();
+local rbp_nXnZ				= get_next();
+
+local rbp_pYpZ				= get_next();
+local rbp_pYnZ				= get_next();
+local rbp_nYpZ				= get_next();
+local rbp_nYnZ				= get_next();
+
+local rbp_pXpYpZ			= get_next();
+local rbp_pXpYnZ			= get_next();
+local rbp_pXnYPz			= get_next();
+local rbp_pXnYnZ			= get_next();
+local rbp_nXpYpZ			= get_next();
+local rbp_nXpYnZ			= get_next();
+local rbp_nXnYPz			= get_next();
+local rbp_nXnYnZ			= get_next();
+
+
+local RBP_SixNeighbors = {
+		[0] = rbp_pY,
+		[1] = rbp_nZ,
+		[2] = rbp_nY,
+		[3] = rbp_nX,
+		[4] = rbp_pX,
+		[5] = rbp_pZ,
+	};
+local MAX_BLOCK_CNT = 256 * 256 * 256;
+
+local function get_vertex(cube,index_1,index_2,index_3)
+	if(not cube)then return end
+	local pos_1;
+	local pos_2;
+	local pos_3;
+	if(cube[index_1] and cube[index_1].position)then
+		pos_1 = cube[index_1].position;
+	end
+	if(cube[index_2] and cube[index_2].position)then
+		pos_2 = cube[index_2].position;
+	end
+	if(cube[index_3] and cube[index_3].position)then
+		pos_3 = cube[index_3].position;
+	end
+	return pos_1,pos_2,pos_3;
+end
 
 function STLExporter:ctor()
-	self.m_bAutoScale = true;
-	self.m_blockAABB = nil;
-	self.m_centerPos = nil;
-	self.m_fScale = 1;
-	self.cube_list = {};
+	LOG.std(nil, "info", "STLExporter", "ctor");
 end
 
 
@@ -145,6 +205,14 @@ end
 
 function STLExporter:init()
 	LOG.std(nil, "info", "STLExporter", "plugin initialized");
+
+	self.m_bAutoScale = true;
+	self.m_blockAABB = nil;
+	self.m_centerPos = nil;
+	self.m_fScale = 1;
+	self.m_nodes = {};
+	self.m_blockModels = {};
+
 	self:RegisterCommand();
 end
 
@@ -177,8 +245,22 @@ function STLExporter:RegisterCommand()
 			self:Export(input_file_name,nil,binary);
 		end,
 	};
+	local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands");
+	Commands["stlexporternative"] = {
+		name="stlexporternative", 
+		quick_ref="/stlexporternative", 
+		desc=[[export a bmax file to stl file use c++ plugin
+/stlexporternative input_file_name
+/stlexporternative input_file_name true
+]], 
+		handler = function(cmd_name, cmd_text, cmd_params, fromEntity)
+			local input_file_name,cmd_text = CmdParser.ParseString(cmd_text);
+			local binary = CmdParser.ParseBool(cmd_text);
+			self:Export(input_file_name,nil,binary,true);
+		end,
+	};
 end
-function STLExporter:Export(input_file_name,output_file_name,binary)
+function STLExporter:Export(input_file_name,output_file_name,binary,native)
 	if(not input_file_name)then return end
 	if(binary == nil)then
 		binary = false;
@@ -189,24 +271,29 @@ function STLExporter:Export(input_file_name,output_file_name,binary)
 		output_file_name = name .. ".stl";
 	end
 	self:Load(input_file_name);
-	if(binary)then
-		self:Export_internal_binary(output_file_name);
+	if(native)then
+		if(ParaScene.BmaxExportToSTL)then
+			if(ParaScene.BmaxExportToSTL(input_file_name,output_file_name,binary))then
+				BroadcastHelper.PushLabel({id="stlexporter", label = format(L"STL文件成功保存到:%s", commonlib.Encoding.DefaultToUtf8(output_file_name)), max_duration=4000, color = "0 255 0", scaling=1.1, bold=true, shadow=true,});
+			end
+		end
 	else
-		self:Export_internal(output_file_name);
+		if(binary)then
+			self:Export_internal_binary(output_file_name);
+		else
+			self:Export_internal(output_file_name);
+		end
+		BroadcastHelper.PushLabel({id="stlexporter", label = format(L"STL文件成功保存到:%s", commonlib.Encoding.DefaultToUtf8(output_file_name)), max_duration=4000, color = "0 255 0", scaling=1.1, bold=true, shadow=true,});
 	end
-	BroadcastHelper.PushLabel({id="stlexporter", label = format(L"STL文件成功保存到:%s", commonlib.Encoding.DefaultToUtf8(output_file_name)), max_duration=4000, color = "0 255 0", scaling=1.1, bold=true, shadow=true,});
-		
-	--if(ParaScene.BmaxExportToSTL(input_file_name,output_file_name,binary))then
-		--BroadcastHelper.PushLabel({id="stlexporter", label = format(L"STL文件成功保存到:%s", commonlib.Encoding.DefaultToUtf8(output_file_name)), max_duration=4000, color = "0 255 0", scaling=1.1, bold=true, shadow=true,});
-	--end
 end
 function STLExporter:Export_internal_binary(output_file_name)
 	ParaIO.CreateDirectory(output_file_name);
 	local face_cont = 6;
-	local function get_vertex(cube,index_1,index_2,index_3)
-		return cube[index_1].position,cube[index_2].position,cube[index_3].position;
-	end
+	
 	local function write_face(file,vertex_1,vertex_2,vertex_3)
+		if(not vertex_1 or not vertex_2 or not vertex_3)then
+			return
+		end
 		local a = vector3d.__sub(vertex_3,vertex_1);
 		local b = vector3d.__sub(vertex_3,vertex_2);
 		local normal = vector3d.__mul(a,b);
@@ -239,12 +326,10 @@ function STLExporter:Export_internal_binary(output_file_name)
 			name = name .. "\0";
 		end
 		file:write(name,total);
-		local cube_num = #self.cube_list;
-		local face_num = cube_num * 12;
-
-		file:WriteInt(face_num);
+		local cnt = self:GetTotalTriangleCnt();
+		file:WriteInt(cnt);
 		local __,cube;
-		for __,cube in ipairs(self.cube_list) do
+		for __,cube in ipairs(self.m_blockModels) do
 			local k;
 			for k = 0,face_cont-1 do	
 				local start_index = k * 4;
@@ -261,10 +346,10 @@ end
 function STLExporter:Export_internal(output_file_name)
 	ParaIO.CreateDirectory(output_file_name);
 	local face_cont = 6;
-	local function get_vertex(cube,index_1,index_2,index_3)
-		return cube[index_1].position,cube[index_2].position,cube[index_3].position;
-	end
 	local function write_face(file,vertex_1,vertex_2,vertex_3)
+		if(not vertex_1 or not vertex_2 or not vertex_3)then
+			return
+		end
 		local a = vector3d.__sub(vertex_3,vertex_1);
 		local b = vector3d.__sub(vertex_3,vertex_2);
 		local normal = vector3d.__mul(a,b);
@@ -282,7 +367,7 @@ function STLExporter:Export_internal(output_file_name)
 		local name = "ParaEngine";
 		file:WriteString(string.format("solid %s\n",name));
 		local __,cube;
-		for __,cube in ipairs(self.cube_list) do
+		for __,cube in ipairs(self.m_blockModels) do
 			local k;
 			for k = 0,face_cont-1 do	
 				local start_index = k * 4;
@@ -297,7 +382,7 @@ function STLExporter:Export_internal(output_file_name)
 		file:close();
 	end
 end
-function STLExporter:GetCube()
+function STLExporter:CreateCube()
 	local m_Vertices = {};
 	local k;
 	for k = g_topLB,g_bkRB do
@@ -370,7 +455,7 @@ function STLExporter:GetCube()
 	m_Vertices[g_bkRB].normal = {0,0,1};
 	return m_Vertices;
 end
-function STLExporter:OffsetPosition(cube,dx,dy,dz,scale)
+function STLExporter:OffsetPosition(cube,dx,dy,dz)
 	if(not cube)then return end
 	local k;
 	for k = g_topLB,g_bkRB do
@@ -378,10 +463,6 @@ function STLExporter:OffsetPosition(cube,dx,dy,dz,scale)
 			cube[k].position[1] = cube[k].position[1] + dx;
 			cube[k].position[2] = cube[k].position[2] + dy;
 			cube[k].position[3] = cube[k].position[3] + dz;
-
-			cube[k].position[1] = cube[k].position[1] * scale;
-			cube[k].position[2] = cube[k].position[2] * scale;
-			cube[k].position[3] = cube[k].position[3] * scale;
 		end
 	end
 end
@@ -390,6 +471,10 @@ function STLExporter:Load(bmax_filename)
 	local xmlRoot = ParaXML.LuaXML_ParseFile(bmax_filename);
 	self:ParseHeader(xmlRoot);
 	self:ParseBlocks(xmlRoot);
+	self:ParseVisibleBlocks();
+	if(self.m_bAutoScale)then
+		self:ScaleModels();
+	end
 end
 function STLExporter:ParseHeader(xmlRoot)
 	if(not xmlRoot)then return end
@@ -409,13 +494,24 @@ function STLExporter:ParseBlocks(xmlRoot)
 	end
 	if(not result)then return end
 	local blocks = commonlib.LoadTableFromString(result[1]);
+	local nodes = {};
 	local aabb = ShapeAABB:new();
 	local k,v;
 	for k,v in ipairs(blocks) do
 		local x = v[1];
 		local y = v[2];
 		local z = v[3];
+		local template_id = v[4];
+		local block_data = v[5];
 		aabb:Extend(x,y,z);
+		local node = {
+			x = x,
+			y = y,
+			z = z,
+			template_id = template_id,
+			block_data = block_data,
+		}
+		table.insert(nodes,node);
 	end
 	self.m_blockAABB = aabb;
 
@@ -425,30 +521,27 @@ function STLExporter:ParseBlocks(xmlRoot)
 	local height = blockMaxY - blockMinY;
 	local depth = blockMaxZ - blockMinZ;
 
+	self.m_centerPos = self.m_blockAABB:GetCenter();
+	self.m_centerPos[1] = (width + 1.0) * 0.5;
+	self.m_centerPos[2] = 0;
+	self.m_centerPos[3]= (depth + 1.0) * 0.5;
+
+
+	local offset_x = blockMinX;
+	local offset_y = blockMinY;
+	local offset_z = blockMinZ;
+
+	local k,node;
+	for k,node in ipairs(nodes) do
+		node.x = node.x - offset_x;
+		node.y = node.y - offset_y;
+		node.z = node.z - offset_z;
+		self:InsertNode(node);
+	end
 	--set scaling;
-	commonlib.echo("============self.m_fScale");
 	if (self.m_bAutoScale) then
 		local fMaxLength = math.max(math.max(height, width), depth) + 1;
 		self.m_fScale = self:CalculateScale(fMaxLength);
-		commonlib.echo(fMaxLength);
-		commonlib.echo(self.m_fScale);
-	end
-
-	local m_centerPos = self.m_blockAABB:GetCenter();
-	m_centerPos[1] = (width + 1.0) * 0.5;
-	m_centerPos[2] = 0;
-	m_centerPos[3]= (depth + 1.0) * 0.5;
-	local k,v;
-	for k,v in ipairs(blocks) do
-		local x = v[1];
-		local y = v[2];
-		local z = v[3];
-		local offset_x = x - blockMinX - m_centerPos[1];
-		local offset_y = y;
-		local offset_z = z - blockMinZ - m_centerPos[3];
-		local cube = self:GetCube();
-		self:OffsetPosition(cube,offset_x,offset_y,offset_z,self.m_fScale);
-		self:PushCube(cube);
 	end
 end
 
@@ -471,10 +564,148 @@ function STLExporter:CalculateScale(length)
 	local nPowerOf2Length = self:NextPowerOf2(length + 0.1);
 	return g_blockSize / nPowerOf2Length;
 end
-function STLExporter:PushCube(cube)
-	if(not cube)then return end
-	table.insert(self.cube_list,cube);
-end
-function STLExporter:TessellateBlock()
 
+function STLExporter:InsertNode(node)
+	if(not node)then return end
+	local index = self:GetNodeIndex(node.x,node.y,node.z);
+	if(index)then
+		self.m_nodes[index] = node;
+	end
+end
+function STLExporter:GetNode(x,y,z)
+	local index = self:GetNodeIndex(x,y,z);
+	if(not index)then
+		return
+	end
+	return self.m_nodes[index];
+end
+function STLExporter:GetNodeIndex(x,y,z)
+	if(x < 0 or y < 0 or z < 0)then
+		return
+	end
+	return x + bit.lshift(z, 8) + bit.lshift(y, 16);
+	--return (DWORD)x + ((DWORD)z << 8) + ((DWORD)y << 16);
+end
+function STLExporter:ParseVisibleBlocks()
+	local k,node;
+	local k;
+	for k = 0,MAX_BLOCK_CNT do
+		local node = self.m_nodes[k];
+		if(node)then
+			local cube = self:TessellateBlock(node.x,node.y,node.z);
+			if(self:GetVerticesCount(cube) > 0)then
+				table.insert(self.m_blockModels,cube);
+			end
+		end
+	end
+end
+function STLExporter:TessellateBlock(x,y,z)
+	local node = self:GetNode(x,y,z);
+	if(not node)then
+		return
+	end
+	local cube = {};
+	local nNearbyBlockCount = 27;
+	local neighborBlocks = {};
+	neighborBlocks[rbp_center] = node;
+	self:QueryNeighborBlockData(x, y, z, neighborBlocks, 1, nNearbyBlockCount - 1);
+	local temp_cube = self:CreateCube();
+	local dx = node.x - self.m_centerPos[1];
+	local dy = node.y - self.m_centerPos[2];
+	local dz = node.z - self.m_centerPos[3];
+	self:OffsetPosition(temp_cube,dx,dy,dz);
+
+	local nFaceCount = 6;
+	local face;
+	for face = 0,nFaceCount - 1 do
+		local nFirstVertex = face * 4;
+		local pCurBlock = neighborBlocks[RBP_SixNeighbors[face]];
+		if(not pCurBlock)then
+			local v;
+			for v = 0,3 do
+				local i = nFirstVertex + v;
+				self:AddVertex(cube,temp_cube, i);
+			end
+		end
+	end
+	return cube;
+end
+function STLExporter:QueryNeighborBlockData(x,y,z,pBlockData,nFrom,nTo)
+	local neighborOfsTable = NeighborOfsTable;
+	local node = self:GetNode(x, y, z);
+	if(not node)then return end
+	local i;
+	for i = nFrom,nTo do
+		local xx = x + neighborOfsTable[i].x;
+		local yy = y + neighborOfsTable[i].y;
+		local zz = z + neighborOfsTable[i].z;
+
+		local pBlock = self:GetNode(xx,yy,zz);
+		local index = i - nFrom + 1;
+		pBlockData[index] = pBlock;
+	end
+end
+function STLExporter:GetVerticesCount(cube)
+	if(not cube)then 
+		return 0;
+	end
+	local cnt = 0;
+	local k;
+	for k = g_topLB,g_bkRB do
+		if(cube[k] and cube[k].position)then
+			cnt = cnt + 1;
+		end
+	end
+	return cnt;
+end
+function STLExporter:ScaleModels()
+	local scale = self.m_fScale;
+	local __,cube;
+	for __,cube in ipairs(self.m_blockModels) do
+		local k;
+		for k = g_topLB,g_bkRB do
+			if(cube[k] and cube[k].position)then
+				cube[k].position[1] = cube[k].position[1] * scale;
+				cube[k].position[2] = cube[k].position[2] * scale;
+				cube[k].position[3] = cube[k].position[3] * scale;
+			end
+		end
+	end
+end
+function STLExporter:AddVertex(target_cube,source_cube,index)
+	if(not target_cube or not source_cube)then
+		return
+	end
+	target_cube[index] = target_cube[index] or {};
+	target_cube[index].position = {
+		source_cube[index].position[1],
+		source_cube[index].position[2],
+		source_cube[index].position[3],
+	};
+	target_cube[index].nomral= {
+		source_cube[index].normal[1],
+		source_cube[index].normal[2],
+		source_cube[index].normal[3],
+	};
+end
+function STLExporter:GetTotalTriangleCnt()
+	local face_cont = 6;
+	local cnt = 0;
+	local __,cube;
+	for __,cube in ipairs(self.m_blockModels) do
+		local k;
+		for k = 0,face_cont-1 do	
+			local start_index = k * 4;
+			local vertex_1,vertex_2,vertex_3 = get_vertex(cube,start_index + 0,start_index + 1,start_index + 2);
+			if(vertex_1)then
+				cnt = cnt + 1;
+			end
+
+			local vertex_1,vertex_2,vertex_3 = get_vertex(cube,start_index + 0,start_index + 2,start_index + 3);
+			if(vertex_1)then
+				cnt = cnt + 1;
+			end
+		end
+	end	
+	return cnt;
 end
